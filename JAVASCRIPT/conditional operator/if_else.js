3195732533fce602b3caa94d85bffe5f37d124a1
rajeshkumarojha-dev <rajeshkumarojha.dev@gmail.com>
2024-12-21 01:22:34 +0530

    update

---
+++ JAVASCRIPT/conditional operator/if_else.js
@@ -0,0 +1,6 @@
+let age=20;
+if (age >= 18) {
+    console.log(`you can vote${age}`)
+} else {
+    console.log('you can"t vote')
+}
--- JAVASCRIPT/var,let,const,typeOf/script.js
+++ JAVASCRIPT/var,let,const,typeOf/script.js
@@ -1,2 +1,11 @@
-str='rajesh'
-console.log(str)
+a=0
+b=Boolean(a)
+console.log(typeof(a))
+console.log(typeof(b))
+console.log(Boolean(1))
+console.log(Boolean(0))
+
+str='Rajesh'
+b=Number(str)
+console.log(typeof(str))
+console.log(typeof(b),b)
